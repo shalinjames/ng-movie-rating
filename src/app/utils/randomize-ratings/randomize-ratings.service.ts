d88920460afa5654ddad4a7c6d6ba0bccce2f7d1
Shalin James <shalin.james@gmail.com>
2018-12-08 18:40:57 +0530

    fix(randomRatingService): change randomizer rules
    Changes to the randomizer rules include:
    - In the given sample set give chance to all the element once per iteration
    - Give different for the seleted element rating each time
    - reset the iteration cycle for each invocation

--- src/app/utils/randomize-ratings/randomize-ratings.service.ts
+++ src/app/utils/randomize-ratings/randomize-ratings.service.ts
@@ -8,12 +8,14 @@ import { UpdateMovie } from "../../store";
   providedIn: "root"
 })
 export class RandomizeRatingsService {
-  public movieIndicies = [];
   public movies: Movie[];
   public randomizeIntervalFunction;
   private timeInterval: number = 1000;
+  public randomizedMovieIndicies;
 
-  constructor(public store: Store) {}
+  constructor(public store: Store) {
+    this.randomizedMovieIndicies = [];
+  }
 
   generateRandomNumber(max: number) {
     return Math.floor(Math.random() * max + 1);
@@ -21,39 +23,55 @@ export class RandomizeRatingsService {
 
   getRandomMovie() {
     const randomNum = this.generateRandomNumber(this.movies.length);
-    return this.movies.find(movie => parseInt(movie.id) === randomNum);
+    if (this.randomizedMovieIndicies.length === this.movies.length) {
+      this.randomizedMovieIndicies.length = 0;
+    }
+    if (this.randomizedMovieIndicies.includes(randomNum)) {
+      return this.getRandomMovie();
+    } else {
+      this.randomizedMovieIndicies.push(randomNum);
+      return this.movies.find(movie => parseInt(movie.id) === randomNum);
+    }
   }
 
   getRandomRating(movie) {
     const randomNum = this.generateRandomNumber(5);
     if (randomNum === movie.ratings) {
-      this.getRandomRating(movie);
+      return this.getRandomRating(movie);
     }
     return randomNum;
+  }
+
+  randomizeMovies() {
+    const movie = this.getRandomMovie();
+    const rating = this.getRandomRating(movie);
+    console.log({
+      title: movie.title,
+      oldRating: movie.ratings,
+      newRating: rating
+    });
+    movie.ratings = rating;
+    this.store.dispatch(new UpdateMovie(movie));
+  }
+
+  reset() {
+    this.movies.length = 0;
+    this.randomizedMovieIndicies.length = 0;
   }
 
   timer(randomizeActivate) {
     if (randomizeActivate) {
       this.randomizeIntervalFunction = setInterval(() => {
-        const movie = this.getRandomMovie();
-        if (movie) {
-          const rating = this.getRandomRating(movie);
-          console.log({
-            title: movie.title,
-            oldRating: movie.ratings,
-            newRating: rating
-          });
-          movie.ratings = rating;
-          this.store.dispatch(new UpdateMovie(movie));
-        }
+        this.randomizeMovies();
       }, this.timeInterval);
     } else {
+      this.reset();
       clearInterval(this.randomizeIntervalFunction);
     }
   }
 
   public randomize(movies, randomizeActivate) {
-    this.movies = movies;
+    this.movies = [...movies];
     this.timer(randomizeActivate);
   }
 }
